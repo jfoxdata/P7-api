1e488dbc1d666da4a31b1447596d8744233471a1
jfoxdata <78475138+jfoxdata@users.noreply.github.com>
2021-04-19 20:32:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,45 +22,45 @@ from flask import Flask, request, jsonify, render_template
 
 app=Flask(__name__)
 
-data = pd.read_csv('data1.csv', index_col=0)
+# data = pd.read_csv('data1.csv', index_col=0)
 
 
-X = data.iloc[:307507]
-X_test = data.iloc[307508:]
+# X = data.iloc[:307507]
+# X_test = data.iloc[307508:]
 
 
-scaler = StandardScaler()
-# X_train = pd.DataFrame(scaler.fit_transform(X_fill), columns= X.columns)
-# X_test = pd.DataFrame(scaler.transform(X_t), columns= test.columns)
-X_train = scaler.fit_transform(X)
-X_s = scaler.transform(X_test)
+# scaler = StandardScaler()
+# # X_train = pd.DataFrame(scaler.fit_transform(X_fill), columns= X.columns)
+# # X_test = pd.DataFrame(scaler.transform(X_t), columns= test.columns)
+# X_train = scaler.fit_transform(X)
+# X_s = scaler.transform(X_test)
 
-M = np.concatenate((X_train, X_s))
+# M = np.concatenate((X_train, X_s))
 
-model = pickle.load(open('model.pkl', 'rb'))
+# model = pickle.load(open('model.pkl', 'rb'))
 
-y_pred = model.predict_proba(M)
+# y_pred = model.predict_proba(M)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST', 'GET'])
+# @app.route('/predict', methods=['POST', 'GET'])
+# # def predict():
+
+# # 	client = NumberInput(min=0, max=len(train_test))
+# # 	prediction = model.predict_proba(client)
+# # 	return render_template('index.html', prediction_text='Your Rating is: {}'.format(predict))
+
 # def predict():
+#     features = [int(x) for x in request.form.values()]
+# #     final_features = M.iloc[features]
+# #     prediction = model.predict_proba(final_features)
+#     prediction = y_pred[features]
 
-# 	client = NumberInput(min=0, max=len(train_test))
-# 	prediction = model.predict_proba(client)
-# 	return render_template('index.html', prediction_text='Your Rating is: {}'.format(predict))
+#     output = prediction
 
-def predict():
-    features = [int(x) for x in request.form.values()]
-#     final_features = M.iloc[features]
-#     prediction = model.predict_proba(final_features)
-    prediction = y_pred[features]
-
-    output = prediction
-
-    return render_template('index.html', prediction_text='Your Rating is: {}'.format(output))
+#     return render_template('index.html', prediction_text='Your Rating is: {}'.format(output))
 
 if __name__=='__main__':
     app.run(debug=True)
