3553655b814a13198a8cea845c2096fe2c1d15c5
jfoxdata <78475138+jfoxdata@users.noreply.github.com>
2021-04-19 19:48:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,62 +15,15 @@ import re
 
 from flask import Flask, request, jsonify, render_template
 
-# One-hot encoding for categorical columns with get_dummies
-def one_hot_encoder(df, nan_as_category = True):
-    original_columns = list(df.columns)
-    categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
-    df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
-    new_columns = [c for c in df.columns if c not in original_columns]
-    return df, new_columns
 
 
-
-def application_train_test(num_rows = None, nan_as_category = False):
-    # Read data and merge
-    df = pd.read_csv('application_train.csv', nrows= num_rows)
-    test_df = pd.read_csv('application_test.csv', nrows= num_rows)
-    #print("Train samples: {}, test samples: {}".format(len(df), len(test_df)))
-    df = df.append(test_df).reset_index()
-    # Optional: Remove 4 applications with XNA CODE_GENDER (train set)
-    df = df[df['CODE_GENDER'] != 'XNA']
-    
-    # Categorical features with Binary encode (0 or 1; two categories)
-    for bin_feature in ['CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY']:
-        df[bin_feature], uniques = pd.factorize(df[bin_feature])
-    # Categorical features with One-Hot encode
-    df, cat_cols = one_hot_encoder(df, nan_as_category)
-    
-    # NaN values for DAYS_EMPLOYED: 365.243 -> nan
-    df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace= True)
-    # Some simple new features (percentages)
-    df['DAYS_EMPLOYED_PERC'] = df['DAYS_EMPLOYED'] / df['DAYS_BIRTH']
-    df['INCOME_CREDIT_PERC'] = df['AMT_INCOME_TOTAL'] / df['AMT_CREDIT']
-    df['INCOME_PER_PERSON'] = df['AMT_INCOME_TOTAL'] / df['CNT_FAM_MEMBERS']
-    df['ANNUITY_INCOME_PERC'] = df['AMT_ANNUITY'] / df['AMT_INCOME_TOTAL']
-    df['PAYMENT_RATE'] = df['AMT_ANNUITY'] / df['AMT_CREDIT']
-    del test_df
-    gc.collect()
-    return df
-
-train_test = application_train_test(num_rows = None, nan_as_category = False)
-# #bureau_balance = bureau_and_balance(num_rows = None, nan_as_category = True)
-# #prev_appli = previous_applications(num_rows = None, nan_as_category = True)
-# #poscash = pos_cash(num_rows = None, nan_as_category = True)
-# #install_pay = installments_payments(num_rows = None, nan_as_category = True)
-# #cre_card_bal = credit_card_balance(num_rows = None, nan_as_category = True)
-# home_cred = pd.read_csv('HomeCredit_columns_description.csv', encoding ='cp1258')
-
-train_test = train_test.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
-
-train = train_test[train_test['TARGET'].notna()].drop(columns=['index', 'SK_ID_CURR'])
-test = train_test[train_test['TARGET'].isna()].drop(columns=['index', 'SK_ID_CURR', 'TARGET'])
+data = pd.read_csv('data1.csv')
 
 
 X = train.drop(columns=['TARGET']) 
 y = train['TARGET']
 
-X_fill = X.fillna(X.mean())
-X_t = test.fillna(X.mean())
+
 
 scaler = StandardScaler()
 # X_train = pd.DataFrame(scaler.fit_transform(X_fill), columns= X.columns)
